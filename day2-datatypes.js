d20748b1c0b68bd8dfacd7f7c808eb731f475427
selimbiber <117529414+selimbiber@users.noreply.github.com>
2023-05-09 23:08:35 +0300

    Day2: Exercise Level 2 has done

--- day2-datatypes.js
+++ day2-datatypes.js
@@ -494,4 +494,72 @@ console.log(challengeName.repeat(2)); // -> 30 Days Of JavaScript30 Days Of Java
 
 /* Day 2: Exercises: Level 2 */
 
+// 1. -> Using console.log() print out the following statement: The quote 'There is no exercise better for the heart than reaching down and lifting people up.' by John Holmes teaches us to help one another.
+console.log("The quote 'There is no exercise better for the heart than reaching down and lifting people up.' by John Holmes teaches us to help one another."); // Çift tırnak içinde tek tırnaklı ifade olabilir!
+
+// 2. -> Using console.log() print out the following quote by Mother Teresa: "Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead."
+console.log('Love is not patronizing and charity isn\'t about pity, it is about love. Charity and love are the same -- with charity you give love, so don\'t just give money but reach out your hand instead.');
+
+// 3. -> Check if typeof '10' is exactly equal to 10. If not make it exactly equal.
+console.log('10' == 10); // -> true
+console.log('10' === 10); // -> false
+console.log('10' === '10'); // -> true
+console.log(10 === 10); // -> true
+
+// 4. -> Check if parseFloat('9.8') is equal to 10 if not make it exactly equal with 10.
+let pf = '9.8';
+let equal10 = parseFloat(pf);
+
+console.log(equal10) // 9.8
+console.log(Math.ceil(equal10)) // 10
+console.log(Math.ceil(equal10)===10) // -> true
+
+// 5. -> Check if 'on' is found in both python and jargon
+let pn = 'python';
+let jn = 'jargon';
+console.log(pn.includes('on')); // -> true
+console.log(jn.includes('on')); // -> true
+
+// 6. -> I hope this course is not full of jargon. Check if jargon is in the sentence.
+let checkJargon = 'I hope this course is not full of jargon.'
+console.log( checkJargon.match('jargon')); // -> ['jargon', index: 34, input: 'I hope this course is not full of jargon.', groups: undefined]
+
+// 7. -> Generate a random number between 0 and 100 inclusively.
+const zeroToHundred = Math.floor(Math.random() * 101)
+console.log(zeroToHundred);
+
+// 8. -> Generate a random number between 50 and 100 inclusively.
+const fiftyToHundred = Math.floor(Math.random() * (100 - 50 + 1)) + 50;
+console.log(fiftyToHundred);
+
+// 9. -> Generate a random number between 0 and 255 inclusively.
+const zeroTo255 = Math.floor(Math.random() * 256)
+console.log(zeroTo255);
+
+// 10. -> Access the 'JavaScript' string characters using a random number.
+let favLanguage = 'JavaScript';
+console.log(favLanguage[0]) // -> J
+
+// 11. -> Use console.log() and escape characters to print the following pattern.
+/*
+1 1 1 1 1
+2 1 2 4 8
+3 1 3 9 27
+4 1 4 16 64
+5 1 5 25 125
+*/
+let escapeChar = '1 1 1 1 1\n2 1 2 4 8 \n3 1 3 9 27\n4 1 4 16 64\n5 1 5 25 125'
+console.log(escapeChar);
+/* ->
+1 1 1 1 1
+2 1 2 4 8 
+3 1 3 9 27
+4 1 4 16 64
+5 1 5 25 125
+*/
+
+// 12. -> Use substr to slice out the phrase because because because from the following sentence:'You cannot end a sentence with because because because is a conjunction'
+let because = 'You cannot end a sentence with because because because is a conjunction';
+console.log(because.substr(31, 24)) // -> because because because
+
 /* Day 2: Exercises: Level 3 */

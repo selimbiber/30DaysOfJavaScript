0b880e9f9ab83211f54fd4a44011589edd522fde
selimbiber <117529414+selimbiber@users.noreply.github.com>
2023-05-16 22:05:52 +0300

    Complete Day 4: Exercises of Conditionals

--- day4-conditionals.js
+++ day4-conditionals.js
@@ -186,6 +186,175 @@ isRaining
 
 //! Exercises: Level 1
 
+//? 1. -> Get user input using prompt(“Enter your age:”). If user is 18 or older , give feedback:'You are old enough to drive' but if not 18 give another feedback stating to wait for the number of years he needs to turn 18.
+/*
+    Enter your age: 30
+    You are old enough to drive.
+
+    Enter your age:15
+    You are left with 3 years to drive.
+*/
+// let yourAge = prompt('Enter your age: ');
+// let enoughAge = 18;
+// let waitYears = enoughAge - yourAge;
+
+// if (yourAge >= enoughAge) {
+//     console.log('You are old enough to drive.');
+// } else {
+//     console.log('You are not old enough to drive, you must wait for ' + waitYears + ' years.');
+// }
+
+//? 2. -> Compare the values of myAge and yourAge using if … else. Based on the comparison and log the result to console stating who is older (me or you). Use prompt(“Enter your age:”) to get the age as input.
+/*
+    Enter your age: 30
+    You are 5 years older than me.
+*/
+// let myAge = 23;
+// let yourAge = prompt('Enter your age:');
+
+// if (myAge > yourAge) {
+//     console.log('I am older than you.');
+// } else {
+//     console.log('I am younger than you or we are the same age.')
+// }
+
+//? 3. -> If a is greater than b return 'a is greater than b' else 'a is less than b'. Try to implement it in to ways: using if else and ternary operator.
+
+// let a = Number(prompt('Enter a value'));
+// let b = Number(prompt('Enter another more value'));
+
+// using if else:
+
+// if (a > b) {
+//     console.log('a is greater than b');
+// } else {
+//     console.log('a is less than b');
+// }
+
+// using ternary operator:
+
+// a > b ? console.log('a is greater than b') : console.log('a is less than b');
+
+//? 4. -> Even numbers are divisible by 2 and the remainder is zero. How do you check, if a number is even or not using JavaScript?
+/*
+    Enter a number: 2
+    2 is an even number
+
+    Enter a number: 9
+    9 is is an odd number.
+*/
+// let number = Number(prompt('Enter a number'));
+
+// if (number % 2 === 0) {
+//     console.log(number + ' is an even number.');
+// } else {
+//     console.log(number + ' is not an odd number.');
+// }
+
 //! Exercises: Level 2
 
+//? 1. -> Write a code which can give grades to students according to theirs scores:
+/*
+    80-100, A
+    70-79, B
+    60-69, C
+    50-59, D
+    0-49, F
+*/
+
+// let yourScore = Number(prompt('Enter your score'));
+
+// if (yourScore >= 80 && yourScore <= 100) {
+//     console.log(yourScore + ' = A');
+// } else if (yourScore >= 70 && yourScore <= 79) {
+//     console.log(yourScore + ' = B');
+// } else if (yourScore >= 60 && yourScore <= 69) {
+//     console.log(yourScore + ' = C');
+// } else if (yourScore >= 50 && yourScore <= 59) {
+//     console.log(yourScore + ' = D');
+// } else if (yourScore >= 0 && yourScore <= 49) {
+//     console.log(yourScore + ' = F')
+// } else {
+//     console.log('You entered the wrong value.')
+// }
+
+//? 2. -> Check if the season is Autumn, Winter, Spring or Summer. If the user input is :
+/*
+    September, October or November, the season is Autumn.
+    December, January or February, the season is Winter.
+    March, April or May, the season is Spring
+    June, July or August, the season is Summer
+*/
+// let month = prompt('Enter the current month');
+
+// if (month === 'September' || month === 'October' || month === 'November') {
+//     console.log('This month is Autumn season.')
+// } else if (month === 'December' || month === 'January' || month === 'February') {
+//     console.log('This month is Winter season.');
+// } else if (month === 'March' || month === 'April' || month === 'May') {
+//     console.log('This month is Spring season.');
+// } else if (month === 'June' || month === 'July' || month === 'August') {
+//     console.log('This month is Summer season.')
+// } else {
+//     console.log('You entered the wrong month.')
+// }
+
+//? 3. -> Check if a day is weekend day or a working day. Your script will take day as an input.
+/*
+  What is the day  today? Saturday
+  Saturday is a weekend.
+
+  What is the day today? saturDaY
+  Saturday is a weekend.
+
+  What is the day today? Friday
+  Friday is a working day.
+
+  What is the day today? FrIDAy
+  Friday is a working day.
+*/
+let today = prompt('What is the day today?');  // -> Kullanıcıdan gün bilgisini al.
+let firstLetter = today[0].toUpperCase(); // -> Gün bilgisinin ilk harfini büyük yap.
+let sliceLetters = today.slice(1) // -> İlk harfin dışındaki kalan harfleri al.
+let otherLetters = sliceLetters.toLowerCase() // -> ve o kalan harfleri küçük harf yap.
+let currentDay = firstLetter + otherLetters; // -> Büyük ilk harf ile küçük diğer harfleri birleştir.
+
+// if (currentDay == 'Saturday' || currentDay == 'Sunday') {
+//     console.log(currentDay + ' is a weekend.')
+// } else if (currentDay == 'Monday' || currentDay == 'Tuesday' || currentDay == 'Wednesday' || currentDay == 'Thursday' || currentDay == 'Friday') {
+//     console.log(currentDay + ' is a working day.');
+// } else {
+//     console.log('There is no such day!')
+// }
+
 //! Exercises: Level 3
+
+//? -> Write a program which tells the number of days in a month, now consider leap year.
+/*
+  Enter a month: January
+  January has 31 days.
+
+  Enter a month: JANUARY
+  January has 31 days.
+
+  Enter a month: February
+  February has 28 days.
+
+  Enter a month: FEbruary
+  February has 28 days.
+*/
+let month = prompt('Enter a month:');  // -> Kullanıcıdan Ay bilgisini al.
+let first_Letter = month[0].toUpperCase(); // -> Ay bilgisinin ilk harfini büyük yap.
+let slice_Letters = month.slice(1) // -> İlk harfin dışındaki kalan harfleri al.
+let other_Letters = slice_Letters.toLowerCase() // -> ve o kalan harfleri küçük harf yap.
+let current_Month = first_Letter + other_Letters; // -> Büyük ilk harf ile küçük diğer harfleri birleştir.
+
+// if (current_Month == 'January' || current_Month == 'March' || current_Month == 'May' || current_Month == 'July' || current_Month == 'August' || current_Month == 'October') {
+//     console.log(current_Month + ' has 31 days.')
+// } else if (current_Month == 'April' || current_Month == 'June' || current_Month == 'September' || current_Month == 'November') {
+//     console.log(current_Month + ' has 30 days.')
+// } else if (current_Month == 'February') {
+//     console.log(current_Month + ' has 28 days. (2023)')
+// } else {
+//     console.log('There is no such month!')
+// }

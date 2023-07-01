0a232ac48d3dc55f2b4b47fd7ad933f75471063d
selimbiber <117529414+selimbiber@users.noreply.github.com>
2023-07-01 22:24:49 +0300

    Update_1.7-23 Day20 (Writing Clean Code)

---
+++ Day20-writing-clean-code.js
@@ -0,0 +1,12 @@
+/*** Day 20 = Writing Clean Code
+ *? https://30dayjavascript.js.org/20-day/
+ *? https://github.com/bsonmez/clean-code-javascript/tree/turkish-translation
+ *? https://readclub.me/how-to-write-javascript-clean-code-and-clean-coding-practices/
+ *? https://medium.com/geekculture/writing-clean-javascript-es6-edition-834e83abc746
+ *? https://www.freecodecamp.org/news/snake-case-vs-camel-case-vs-pascal-case-vs-kebab-case-whats-the-difference/
+ *! Not: Kod bloklarını yorum satırından çıkarmak için (ctrl + k + u) kısayolunu kullanabilirsin.
+***/
+
+/*
+    “Any fool can write code that a computer can understand. Good programmers write code that humans can understand.” — Martin Fowler.
+*/
